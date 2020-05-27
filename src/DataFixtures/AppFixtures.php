bb7cf150dc495de6c676f0a1044422c5df03b31a
nyasn <rnyasn@gmail.com>
2020-05-27 15:20:19 +0300

    login/register/qrcode

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,13 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $user = new User();
+       /* $user = new User();
         $user->setEmail('admin');
         $user->setRoles(['ROLE_ADMIN']);
         $password = $this->encoder->encodePassword($user, 'admin');
         $user->setPassword($password);
 
         $manager->persist($user);
-        $manager->flush();
+        $manager->flush();*/
     }
 }
