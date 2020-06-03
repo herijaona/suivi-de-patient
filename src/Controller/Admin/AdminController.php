2e6d1ab3155cb793edf6c3e6f8c7710b5f9042d7
alvin <managnora@gmail.com>
2020-06-03 14:48:51 +0200

    admin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,14 +5,51 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin")
      */
     public function index()
     {
-        return $this->render('admin/index.html.twig', [
+        return $this->render('admin/dashboard.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+
+    }
+
+    /**
+     * @Route("/vaccin", name="vaccin_admin")
+     */
+    public function vaccin_admin()
+    {
+        return $this->render('admin/vaccin.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+
+    }
+
+    /**
+     * @Route("/praticien", name="praticiens_admin")
+     */
+    public function praticiens_admin()
+    {
+        return $this->render('admin/praticien.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+
+    }
+
+    /**
+     * @Route("/patient", name="patients_admin")
+     */
+    public function patients_admin()
+    {
+        return $this->render('admin/patient.html.twig', [
             'controller_name' => 'AdminController',
         ]);
 
