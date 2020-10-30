6529c1d3fc03daa81693ed6e2c9a22c43f5c4432
Avo Jaoalison <jaoalison23@gmail.com>
2020-10-30 09:59:43 +0300

    suite

--- src/Controller/Api/ApiController.php
+++ src/Controller/Api/ApiController.php
@@ -107,6 +107,7 @@ class ApiController extends AbstractController
     public function api_profile_edit(EntityManagerInterface $entityManager,Request $request,CityRepository $cityRepository, StateRepository  $stateRepository)
     {
         $patient = json_decode($request->getContent(), true);
+        dd($patient);
         $id = $patient['id'];
         $cityBorn = $patient['cityBorn'];
         if ($cityBorn != null) $cityBorn = $cityRepository->find($cityBorn);
